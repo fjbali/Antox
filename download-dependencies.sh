6f1972e67fc6f5075763cc564d8720337a90d940
Zoff <zoff99@users.noreply.github.com>
2017-01-16 11:09:42 +0100

    download compiled files from CI

--- download-dependencies.sh
+++ download-dependencies.sh
@@ -7,21 +7,20 @@ rm -f app/src/main/jniLibs/armeabi/libtox4j.so
 rm -f app/src/main/jniLibs/x86/libtox4j.so
 rm -f app/src/main/jniLibs/arm64-v8a/libtox4j.so
 
-# wget https://build.tox.chat/job/tox4j_build_android_armel_release/lastSuccessfulBuild/artifact/artifacts/libtox4j.so -O app/src/main/jniLibs/armeabi/libtox4j.so
-# wget https://build.tox.chat/job/tox4j_build_android_x86_release/lastSuccessfulBuild/artifact/artifacts/libtox4j.so -O app/src/main/jniLibs/x86/libtox4j.so
-# wget https://build.tox.chat/job/tox4j_build_android_arm64_release/lastSuccessfulBuild/artifact/artifacts/libtox4j.so -O app/src/main/jniLibs/arm64-v8a/libtox4j.so
-
 rm -f app/src/main/jniLibs/armeabi/libkaliumjni.so
 rm -f app/src/main/jniLibs/x86/libkaliumjni.so
 rm -f app/src/main/jniLibs/arm64-v8a/libkaliumjni.so
-
-# wget https://build.tox.chat/job/libkaliumjni_build_android_armel_static_release/lastSuccessfulBuild/artifact/kalium-jni/jni/libkaliumjni.so -O app/src/main/jniLibs/armeabi/libkaliumjni.so
-# wget https://build.tox.chat/job/libkaliumjni_build_android_x86_static_release/lastSuccessfulBuild/artifact/kalium-jni/jni/libkaliumjni.so -O app/src/main/jniLibs/x86/libkaliumjni.so
-# wget https://build.tox.chat/job/libkaliumjni_build_android_arm64_static_release/lastSuccessfulBuild/artifact/kalium-jni/jni/libkaliumjni.so -O app/src/main/jniLibs/arm64-v8a/libkaliumjni.so
 
 cd ${0%/*}
 mkdir -p app/libs
 rm -f app/libs/tox4j_2.11.jar
 # wget https://build.tox.chat/job/tox4j_build_android_armel_release/lastSuccessfulBuild/artifact/artifacts/tox4j_2.11-0.1-SNAPSHOT.jar -O app/libs/tox4j_2.11.jar
+
+REPOUSER="zoff99"
+REPO="Antox"
+BRANCH="z_new_source"
+
+wget 'https://circleci.com/api/v1/project/'"$REPOUSER"'/'"$REPO"'/latest/artifacts/0/$CIRCLE_ARTIFACTS/supplement.zip&branch='"$BRANCH" -O ./supplement.zip
+unzip ./suppl.zip
 
 exit 0
