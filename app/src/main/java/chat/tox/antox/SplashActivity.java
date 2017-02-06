e9246c5dac7c58256357c46159875a5f640d7e57
Zoff <zoff99@users.noreply.github.com>
2017-02-06 16:29:37 +0100

    getter

--- app/src/main/java/chat/tox/antox/SplashActivity.java
+++ app/src/main/java/chat/tox/antox/SplashActivity.java
@@ -36,7 +36,7 @@ public class SplashActivity extends AppCompatActivity implements EasyPermissions
         
         // unpack included nodes file, if not already unpacked
         String baseDir = Context.getFilesDir();
-        String fileName = State.nodeFileNameGlobal;
+        String fileName = State.getNodeFileNameGlobal();
         File nodesfile = new File(baseDir, fileName);
         if (!nodesfile.exists())
         {
